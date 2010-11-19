29f5dc087c8fa9cb9e14ad0531363a0e09cc53d8
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2010-11-19 17:25:16 +0700

    init + add skeleton

---
+++ lib/Data/Format/Pretty/Console.pm
@@ -0,0 +1,31 @@
+package Data::Format::Pretty::Console;
+# ABSTRACT: Pretty-print data structure for console output
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+=back
+
+=cut
+
+use 5.010;
+use strict;
+use warnings;
+
+use Any::Moose;
+use Text::ASCIITable;
+use YAML::Any;
+
+=head1 METHODS
+
+=head1 FAQ
+
+=head1 SEE ALSO
+
+Modules used for formatting: L<Text::ASCIITable>
+
+=cut
+
+1;
